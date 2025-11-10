03ac08bfe09215ef380064b9a702aa587e4cff6e
Jerome Eteve <jerome.eteve@gmail.com>
2025-11-10 20:48:05 +0000

    All query building

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,80 @@
 use pyo3::prelude::*;
+use pyo3::types::PyAnyMethods;
 
 /// Python glue for mokaccino library.
 /// 
 #[pymodule]
 mod mokaccino_py {
-    use pyo3::{prelude::*, types::PyType};
+    use mokaccino::prelude::CNFQueryable;
+    use pyo3::{prelude::*, types::{PyIterator, PyType}};
 
+    #[derive(Clone)]
     #[pyclass]
     pub struct Query(mokaccino::prelude::Query);
 
     #[pymethods]
     impl Query {
         #[classmethod]
-        fn from_kv(cls: &Bound<'_, PyType>, k: String, v: String) -> PyResult<Self> {
-            let query = mokaccino::prelude::Query::term(k, v);
-            Ok(Self(query))
+        fn from_kv(_cls: &Bound<'_, PyType>, k: String, v: String) -> PyResult<Self> {
+            Ok(Self(k.has_value(v)))
         }
+
+        #[classmethod]
+        fn from_kprefix(_cls: &Bound<'_, PyType>, k: String, v: String) -> PyResult<Self> {
+            Ok(Self(k.has_prefix(v)))
+        }
+
+        #[classmethod]
+        fn from_klt(_cls: &Bound<'_, PyType>, k: String, v: i64) -> PyResult<Self> {
+            Ok(Self(k.i64_lt(v)))
+        }
+        
+        #[classmethod]
+        fn from_kle(_cls: &Bound<'_, PyType>, k: String, v: i64) -> PyResult<Self> {
+            Ok(Self(k.i64_le(v)))
+        }
+
+        #[classmethod]
+        fn from_keq(_cls: &Bound<'_, PyType>, k: String, v: i64) -> PyResult<Self> {
+            Ok(Self(k.i64_eq(v)))
+        }
+
+        #[classmethod]
+        fn from_kge(_cls: &Bound<'_, PyType>, k: String, v: i64) -> PyResult<Self> {
+            Ok(Self(k.i64_ge(v)))
+        }
+
+        #[classmethod]
+        fn from_kgt(_cls: &Bound<'_, PyType>, k: String, v: i64) -> PyResult<Self> {
+            Ok(Self(k.i64_gt(v)))
+        }
+
+        #[classmethod]
+        fn from_not(_cls: &Bound<'_, PyType>, q: &Self) -> PyResult<Self> {
+            Ok(Self(! q.0.clone()))
+        }
+
+        #[classmethod]
+        fn from_and(_cls: &Bound<'_, PyType>, iterable: &Bound<'_,PyAny>) -> PyResult<Self> {
+            let mut items: Vec<mokaccino::prelude::Query> = vec![];
+            for item in  PyIterator::from_object(iterable)? {
+                let q: Self = item?.extract::<Query>()?;  
+                items.push(q.0);
+            }
+            Ok(Self(mokaccino::prelude::Query::from_and(items)))
+        }
+
+        #[classmethod]
+        fn from_or(_cls: &Bound<'_, PyType>, iterable: &Bound<'_,PyAny>) -> PyResult<Self> {
+            let mut items: Vec<mokaccino::prelude::Query> = vec![];
+            for item in  PyIterator::from_object(iterable)? {
+                let q: Self = item?.extract::<Query>()?;  
+                items.push(q.0);
+            }
+            Ok(Self(mokaccino::prelude::Query::from_or(items)))
+        }
+        
+
     }
 
     /// Formats the sum of two numbers as string.
---
+++ tests/test_queries.py
@@ -0,0 +1,19 @@
+import mokaccino_py as mp
+from mokaccino_py import Query
+
+def test_final_queries():
+    assert Query.from_kv("field", "value") is not None
+    assert Query.from_kprefix("field", "value") is not None
+    assert Query.from_klt("field", 123) is not None
+    assert Query.from_kle("field", 123) is not None
+    assert Query.from_keq("field", 123) is not None
+    assert Query.from_kge("field", 123) is not None
+    assert Query.from_kgt("field", 123) is not None
+
+def test_combination():
+    q1 = Query.from_kv("f", "v")
+    assert Query.from_not(q1) is not None
+
+    q2 = Query.from_kv("a", "b")
+    assert Query.from_and([q1, q2]) is not None
+    assert Query.from_or([q1, q2]) is not None
